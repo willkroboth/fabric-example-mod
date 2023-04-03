385f6b7d002f13b245c115bdccd41dc144ab7ff7
BuildTools <willkroboth@gmail.com>
2023-04-03 16:35:11 -0400

    Fix PacketListener for 1.19.4

--- src/main/java/net/fabricmc/example/mixin/PacketListener.java
+++ src/main/java/net/fabricmc/example/mixin/PacketListener.java
@@ -3,7 +3,7 @@ package net.fabricmc.example.mixin;
 import io.netty.channel.ChannelHandlerContext;
 import net.fabricmc.example.ExampleMod;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.Packet;
+import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class PacketListener {
-    @Inject(at = @At("HEAD"), method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/Packet;)V")
+    @Inject(at = @At("HEAD"), method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/packet/Packet;)V")
     public void interceptPacket(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
         ExampleMod.LOGGER.info("Intercepted packet");
         ExampleMod.LOGGER.info(packet.getClass().toString());
